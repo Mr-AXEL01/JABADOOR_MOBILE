2dc8eb30e0710d2975e353f2e67f1857458c31b8
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-06-03 15:01:08 +0100

    set tabs for the project with them icons

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,22 +1,49 @@
 import React from 'react'
 import { Tabs } from 'expo-router'
 import Colors from '@/constants/Colors.ts'
+import { FontAwesome5, Ionicons } from '@expo/vector-icons'
 
 const Layout = () => {
   return (
   <Tabs 
     screenOptions={{ 
-        tabBarActiveTintColor: Colors.primary,
-        tabBarLabelStyle: {
-            fontFamily: 'mon-sb',
-            
-        }
+      tabBarActiveTintColor: Colors.primary,
+      tabBarLabelStyle: {
+          fontFamily: 'mon-sb',
+          
+      }
     }}>
     <Tabs.Screen 
-        name="index" 
-        options={{
-            tabBarLabel: 'Explore',
-        }} 
+      name="index" 
+      options={{
+          tabBarLabel: 'Explore',
+          tabBarIcon: ({ color , size }) => 
+            <Ionicons name="search" color={color} size={size} />
+      }} 
+    />
+    <Tabs.Screen 
+      name="wishlists" 
+      options={{
+        tabBarLabel: 'Wishlists',
+        tabBarIcon: ({ color , size }) => 
+          <Ionicons name="heart-outline" color={color} size={size} />
+      }} 
+    />
+    <Tabs.Screen 
+      name="trips" 
+      options={{
+        tabBarLabel: 'Trips',
+        tabBarIcon: ({ color , size }) => 
+          <FontAwesome5 name="airbnb" color={color} size={size} />
+      }} 
+    />
+    <Tabs.Screen 
+      name="profile" 
+      options={{
+        tabBarLabel: 'Profile',
+        tabBarIcon: ({ color , size }) => 
+          <Ionicons name="person-circle-outline" color={color} size={size} />
+      }} 
     />
   </Tabs>
   );
