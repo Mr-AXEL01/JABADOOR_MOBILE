55994e3327918d4f8f13d1ff7c3d17321f1da596
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-06-26 18:55:37 +0100

    update style for the details page

--- app/listing/[Host_code].tsx
+++ app/listing/[Host_code].tsx
@@ -5,7 +5,7 @@ import { useLocalSearchParams, useRouter } from 'expo-router';
 import axios from 'axios';
 import Colors from '@/constants/Colors';
 import { Ionicons } from '@expo/vector-icons';
-import { SvgXml } from 'react-native-svg'; // Import SvgXml for SVG rendering
+import { SvgXml } from 'react-native-svg';
 import { useTranslation } from 'react-i18next';
 import Carousel from 'pinar';
 
@@ -46,48 +46,46 @@ const Page = () => {
     const svgXmlData = amenityIcon.replace('fill: currentcolor;', '');
 
     return (
-      <View key={index} style={styles.amenity}>
+      <View key={amenity.amenity_code} style={styles.amenity}>
         <SvgXml xml={svgXmlData} width={24} height={24} />
         <Text style={styles.amenityText}>{amenityName}</Text>
       </View>
     );
   };
 
-const shareListing = async () => {
-  if (!host || !host.Host_code) {
-    console.error('Host data or Host code is not available.');
-    alert('Host data or Host code is not available.');
-    return;
-  }
+  const shareListing = async () => {
+    if (!host || !host.Host_code) {
+      console.error('Host data or Host code is not available.');
+      alert('Host data or Host code is not available.');
+      return;
+    }
 
-  try {
-    const imageUrl = host.image[0].secure_url;
-    const imageUri = `${FileSystem.cacheDirectory}${host.Host_code}.jpg`;
+    try {
+      const imageUrl = host.image[0].secure_url;
+      const imageUri = `${FileSystem.cacheDirectory}${host.Host_code}.jpg`;
 
-    await FileSystem.downloadAsync(imageUrl, imageUri);
+      await FileSystem.downloadAsync(imageUrl, imageUri);
 
-    await Share.share({
-      message: `Check out this wonderful Host: ${host.nom}\n\nLink: https://main.d11i2xf9qyhgyw.amplifyapp.com/host/${host.Host_code}`,
-      url: imageUri,
-    });
+      await Share.share({
+        message: `Check out this wonderful Host: ${host.nom}\n\nLink: https://main.d11i2xf9qyhgyw.amplifyapp.com/host/${host.Host_code}`,
+        url: imageUri,
+      });
 
-    console.log('Shared successfully');
-  } catch (error) {
-    console.error('Error sharing listing:', error.message);
-    alert(`Error sharing: ${error.message}`);
-  }
-};
-
+      console.log('Shared successfully');
+    } catch (error) {
+      console.error('Error sharing listing:', error.message);
+      alert(`Error sharing: ${error.message}`);
+    }
+  };
 
   return (
     <ScrollView style={styles.container}>
-      {/* <Image source={{ uri: host.image[0].secure_url }} style={styles.mainImage} /> */}
-      <Carousel style={styles.carousel} 
-        showsControls={false} 
+      <Carousel style={styles.carousel}
+        showsControls={false}
         dotStyle={styles.dotStyle}
-        activeDotStyle={[styles.dotStyle, {backgroundColor: 'white'}]}
+        activeDotStyle={[styles.dotStyle, { backgroundColor: 'white' }]}
       >
-        {host.image.map(img=><Image style={styles.images} source={{uri: img.secure_url}} key={host.Host_code} />)}
+        {host.image.map(img => <Image style={styles.images} source={{ uri: img.secure_url }} key={host.Host_code} />)}
       </Carousel>
       <TouchableOpacity style={styles.backButton} onPress={() => router.back()}>
         <Ionicons name="arrow-back" size={24} color="#000" />
@@ -103,35 +101,53 @@ const shareListing = async () => {
 
       <View style={styles.contentContainer}>
         <Text style={styles.title}>{host.nom}</Text>
-        <Text style={styles.description}>{host.About}</Text>
+        <View style={{ flexDirection: 'row', alignItems: 'center', marginBottom: 15 }}>
+          <Ionicons name="location-outline" size={20} color={'#000'} />
+          <Text style={styles.address}>{host.address.address},{host.address.city},{host.address.country}</Text>
+        </View>
         <View style={styles.infoRow}>
           <View style={styles.infoItem}>
-            <Ionicons name="star" size={16} color={Colors.primary} />
+            <Ionicons name="person-outline" size={20} />
+            <Text style={styles.infoText}> {host.guests}</Text>
+          </View>
+          <View style={styles.infoItem}>
+            <Ionicons name="star-outline" size={20} />
             <Text style={styles.infoText}>{host.Rating}</Text>
           </View>
           <View style={styles.infoItem}>
-            <Ionicons name="bed" size={16} color={Colors.primary} />
-            <Text style={styles.infoText}>{t('Bedrooms')}: {host.bedrooms}</Text>
+            <Ionicons name="bed-outline" size={20} />
+            <Text style={styles.infoText}> {host.bedrooms}</Text>
           </View>
           <View style={styles.infoItem}>
-            <Ionicons name="water" size={16} color={Colors.primary} />
-            <Text style={styles.infoText}>{t('Baths')}: {host.baths}</Text>
+            <Ionicons name="water-outline" size={20} />
+            <Text style={styles.infoText}> {host.baths}</Text>
           </View>
         </View>
-        <Text style={styles.amenitiesTitle}>{t('Amenities')}:</Text>
-        {host.amenities && host.amenities.length > 0 ? (
-          host.amenities.map(renderAmenity)
-        ) : (
-          <Text style={styles.noAmenities}>{t('No amenities listed')}</Text>
-        )}
+        <View style={{ margin: 25, marginLeft: 0 }}>
+          <Text style={styles.amenitiesTitle}>{t('Amenities')} :</Text>
+          <Text style={styles.amenitiesDescreption}>{t('About the property\'s amenities and services')}</Text>
+        </View>
+        <View style={styles.amenitiesContainer}>
+          {host.amenities && host.amenities.length > 0 ? (
+            host.amenities.map(renderAmenity)
+          ) : (
+            <Text style={styles.noAmenities}>{t('No amenities listed')}</Text>
+          )}
+        </View>
+        <View style={styles.aboutContainer}>
+          <Text style={styles.about}>{host.About}</Text>
+        </View>
         <View style={styles.hostContainer}>
-          <Image source={{ uri: host.image[0].secure_url }} style={styles.hostImage} />
-          <Text style={styles.hostName}>{t('Hosted by')}: {host.nom}</Text>
+          <Text style={styles.hostCardTitle}>{t('Meet Your Host :')}</Text>
+          <View style={styles.hostCardContainer}>
+            <Image source={{ uri: host.image[0].secure_url }} style={styles.hostImage} />
+            <Text style={styles.hostName}>Abd_elhaq</Text>
+          </View>
         </View>
         <View style={styles.buttonContainer}>
-          <View style={{flexDirection:'row'}}>
+          <View style={{ flexDirection: 'row' }}>
             <Text style={styles.price}>${host.price}</Text>
-            <Text style={{margin:4,}}>night</Text>
+            <Text style={{ margin: 4, }}>night</Text>
           </View>
           <TouchableOpacity style={styles.addButton}>
             <Text style={styles.addButtonText}>{t('Book')}</Text>
@@ -161,8 +177,8 @@ const styles = StyleSheet.create({
   images: {
     width: '100%',
     height: '100%',
-    borderBottomLeftRadius: 20,
-    borderBottomRightRadius: 20,
+    borderBottomLeftRadius: 15,
+    borderBottomRightRadius: 15,
   },
   backButton: {
     position: 'absolute',
@@ -194,29 +210,34 @@ const styles = StyleSheet.create({
   },
   contentContainer: {
     padding: 20,
+    paddingTop: 5,
   },
   title: {
-    fontSize: 24,
-    fontWeight: 'bold',
+    fontSize: 28,
+    fontWeight: '800',
     marginVertical: 10,
+    marginBottom: 15,
   },
-  description: {
-    fontSize: 16,
-    color: '#888',
-    marginBottom: 20,
+  address: {
+    fontSize: 18,
+    fontWeight: '400',
   },
   infoRow: {
     flexDirection: 'row',
-    justifyContent: 'space-around',
-    marginBottom: 20,
+    justifyContent: 'space-between',
+    padding: 10,
+    paddingTop: 20,
+    paddingBottom: 20,
+    borderColor: 'silver',
+    borderBottomWidth: 1,
+    borderTopWidth: 1,
   },
   infoItem: {
     flexDirection: 'row',
     alignItems: 'center',
   },
   infoText: {
-    fontSize: 16,
-    color: '#333',
+    fontSize: 17,
     marginLeft: 5,
   },
   price: {
@@ -225,39 +246,78 @@ const styles = StyleSheet.create({
     marginBottom: 10,
   },
   amenitiesTitle: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    marginTop: 20,
-    marginBottom: 10,
+    fontSize: 20,
+    fontWeight: '500',
+  },
+  amenitiesDescreption: {
+    fontSize: 16,
+    fontWeight: '400',
+  },
+  amenitiesContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    gap: 10,
+    paddingBottom: 15,
   },
   amenity: {
-    flexDirection: 'row',
+    width: '30%',
     alignItems: 'center',
-    marginBottom: 5,
+    justifyContent: 'center',
+    borderWidth: 0.4,
+    padding: 8,
+    borderRadius: 16,
+    marginBottom: 12,
   },
   amenityText: {
-    fontSize: 16,
+    fontSize: 14,
     color: '#333',
-    marginLeft: 5,
+    marginLeft: 8,
   },
   noAmenities: {
     fontSize: 16,
     color: '#888',
     marginBottom: 20,
   },
-  hostContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+  aboutContainer: {
+    padding: 15,
+    borderColor: 'silver',
+    borderBottomWidth: 1,
+    borderTopWidth: 1,
     marginBottom: 20,
   },
+  about: {
+    fontSize: 14,
+    color: '#666',
+  },
+  hostContainer: {
+    padding: 8,
+    borderBottomWidth: 0.5,
+    marginBottom: 20,
+  },
+  hostCardTitle: {
+    fontSize: 20,
+    fontWeight: '600',
+  },
+  hostCardContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 0.5,
+    margin: 25,
+    marginLeft: 45,
+    padding: 15,
+    width: '75%',
+    height: 170,
+    borderRadius: 16,
+    elevation: 8,
+    backgroundColor: '#f5f5f5',
+  },
   hostImage: {
-    width: 40,
-    height: 40,
-    borderRadius: 20,
-    marginRight: 10,
+    width: 90,
+    height: 90,
+    borderRadius: 50,
   },
   hostName: {
-    fontSize: 16,
+    fontSize: 20,
     color: '#333',
   },
   buttonContainer: {
