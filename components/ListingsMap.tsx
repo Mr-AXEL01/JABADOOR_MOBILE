e3db1fd842951d1cc03bd3cd4ad8f76947a622f2
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-06-27 11:45:26 +0100

    install the react-native-maps package and set a component for Listings in Map

---
+++ components/ListingsMap.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+import MapView from 'react-native-maps';
+
+const ListingsMap = () => {
+  return (
+    <View>
+      <Text>ListingsMap</Text>
+    </View>
+  )
+}
+
+export default ListingsMap
